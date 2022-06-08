0fea8c4a94108f7d67812e9f85ae1757e5fdcd7f
hebert <hebert.devs@gmail.com>
2022-06-08 18:44:52 -0300

    completed project

--- script.js
+++ script.js
@@ -14,6 +14,16 @@ let posicaoHr = (hr*360/12)+(min*(360/60)/12);
 let posicaoMin = (min*360/60)+(seg*(360/60)/60);
 let posicaoSeg = seg*360/60;
 
-PONTEIROHORA.style.transform = "rotate(" + posicaoHr + "deg)"
-PONTEIROMINUTO.style.transform = "rotate(" + posicaoMin + "deg)"
-PONTEIROSEGUNDO.style.transform =  "rotate(" + posicaoSeg + "deg)"
+function executarRelogio(){
+
+  posicaoHr = posicaoHr+(3/360);
+  posicaoMin = posicaoMin+(6/60);
+  posicaoSeg = posicaoSeg+6;
+
+  PONTEIROHORA.style.transform = "rotate(" + posicaoHr + "deg)";
+  PONTEIROMINUTO.style.transform = "rotate(" + posicaoMin + "deg)";
+  PONTEIROSEGUNDO.style.transform =  "rotate(" + posicaoSeg + "deg)";
+
+  
+}
+var intervalo = setInterval(executarRelogio, 1000);
